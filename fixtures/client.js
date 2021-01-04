f871d254d9ed1bf77bddd333bba58722afe71bbc
Tony Coop <tony@thewholeworldwindow.co.uk>
2021-01-04 19:11:43 +0000

    Add client to seed.

--- fixtures/client.js
+++ fixtures/client.js
@@ -2,7 +2,16 @@ import faker from 'faker'
 import Client from '../src/models/ClientModel'
 
 export default async () => {
-  for (let i = 0; i < 10; i++) {
+  const client = new Client({ name: 'Test client' })
+
+  await client
+    .save()
+    .then(success => null)
+    .catch(err => {
+      console.log(err)
+    })
+
+  for (let i = 0; i < 4; i++) {
     const client = new Client({
       name: faker.company.companyName(),
     })
